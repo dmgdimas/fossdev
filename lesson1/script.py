67cbc1477ac60be9c8a698f03c24e4140be43dc1
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-02-05 14:50:27 +0800

    Added string check

--- lesson1/script.py
+++ lesson1/script.py
@@ -2,6 +2,8 @@ def sum(a,b):
     return a+b
 
 def devide(a,b):
+    if isinstance(a,str) or isinstance(b,str):
+	raise ValueError("You can not divide strings!")
     if b!=0:
         return a/b
     else:
