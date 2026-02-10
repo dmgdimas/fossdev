8f631df385c479d0670b294ba2e02823aaf498fc
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-02-10 14:38:04 +0800

    added list division check

--- lesson1/script.py
+++ lesson1/script.py
@@ -4,6 +4,8 @@ def sum(a,b):
 def devide(a,b):
     if isinstance(a,str) or isinstance(b,str):
         raise ValueError("You can not divide strings!")
+    if isinstance(a,list) or isinstance(b,list):
+        raise ValueError("You can not divide lists")
     if b!=0:
         return a/b
     else:
