28f9a4a9a22204a64e98064818cbbd7a6ea9a330
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-03-05 14:17:26 +0800

    added tax_calculator

--- lesson1/testing/principle/src/math_demo.py
+++ lesson1/testing/principle/src/math_demo.py
@@ -7,3 +7,9 @@ def add_with_bug(a, b):
 def tax_calculator_bugged(income):
     tax_rate = 0.2
     return int(income * tax_rate * 100 + 0.5) / 100  
+
+def tax_calculator(income):
+    if income < 0:
+        raise ValueError("Income cannot be negative")
+    tax_rate = 0.2
+    return int(income * tax_rate * 100 + 0.5) / 100  
