458140e522e3a944dbeadea927389b15626d04e8
Dmitriy Gorbachev <zibelmand@gmail.com>
2026-03-05 13:12:17 +0800

    Changed test_principles.py

--- lesson1/testing/principle/tests/test_principles.py
+++ lesson1/testing/principle/tests/test_principles.py
@@ -1,10 +1,31 @@
-# TODO make if with pip install -e .
+#TODO make it with pip install -e .
+# in project root_dir after setup.py defined
 
-from math_demo import add
+
+# Раннее тестирование позволяет сэкономить время позднее
+# Тесты показывают наличие ошибок, а не отсутсвие
+# Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать новые ошибки (pescicide paradox)
+# Тесты покрывают как успешные, так и ошибочные кейсы
+
+from math_demo import add, add_with_bug
+
 
 def test_addition():
-    assert 2 + 2 == 4
-    print("Test ADDITION PASSED")
+    assert add(2,2) == 4
+    assert add(0,0) == 0
+    assert add(7,6) == 13
+    print("Test addition passed")
+
+
+def test_addition_with_bug():
+    assert add_with_bug(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    assert add_with_bug(7,6) == 13
+    print("Test bugged addition passed")
+
 
 if __name__ == "__main__":
     test_addition()
